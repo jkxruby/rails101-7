623bbc9dc695f2f983b9799e5e2597d1d2ac8ec3
jkxruby <jkxruby@gmail.com>
2017-07-06 15:22:40 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-before_action :authenticate_user! , only: [:new, :edit, :destroy, :update, :create]
+before_action :authenticate_user! , only: [:new, :edit, :destroy, :update, :create, :join, :quit]
 before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
 def new
@@ -40,6 +40,29 @@ def destroy
   @group.destroy
   redirect_to groups_path,alert: "删了，但要快速开发rails101！"
 end
+
+def join
+  @group = Group.find(params[:id])
+  if !current_user.is_member_of?(@group)
+    current_user.join!(@group)
+    flash[:notice] = "加入本版成功！但要快速开发！"
+  else
+    flash[:warning] = "你已经是本讨论版成员了！"
+  end
+  redirect_to group_path(@group)
+end
+
+def quit
+  @group = Group.find(params[:id])
+  if current_user.is_member_of?(@group)
+    current_user.quit!(@group)
+    flash[:alert] = "已经退出本讨论版！"
+  else
+    flash[:warning] = "本来就不是，怎么退！快速开发！"
+  end
+  redirect_to group_path(@group)
+end
+
 
 private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 resources :groups do
+  member do
+    post :join
+    post :quit
+  end
+end
+
+resources :groups do
   resources :posts
 end
 
